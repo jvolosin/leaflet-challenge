18871d5cb031ebccc952622e7a5c6bb9bd961443
GIS Warrior <28575212+jvolosin@users.noreply.github.com>
2021-02-04 08:07:45 -0500

    creating markers and popup info

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,7 +1,7 @@
 // Creating map object
 var myMap = L.map("map", {
-  center: [40.7, -73.95],
-  zoom: 11
+  center: [-15.92, -56.75],
+  zoom: 4
 });
 
 // Adding tile layer to the map
@@ -14,12 +14,51 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
+
 var usgsURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
+
+//Set colors based on depth
+var colors = ["#33FFC3", "#26BF92", "#1A8062", "#0D4031", "#062018"]
+var depth = [0, 30, 50, 70, 90];
 
 //Request Data
-d3.json(usgsURL, function(data) {
-  createFeatures(data.features);
-});
+d3.json(usgsURL, function(response) {
+  console.log(response);
 
+  var usgsURL = response.features;
+
+  function getColor(d) {
+    for (var i = 0; i < depth.length; i++) {
+      if (d < depth[i]) {
+        return colors[i - 1];
+      }
+      else if (d > depth[depth.length - 1]) {
+        return colors[depth.length - 1];
+      }
+    }
+  }
+
+
+  // Create loop
+  for (var i = 0; i < usgsURL.length; i++) {
+
+    
+    var location = usgsURL[i].geometry;
+
+    //Create markers and popup info
+    
+    if (location) {
+      L.circleMarker([location.coordinates[1], location.coordinates[0]], {
+        fillOpacity: 0.65,
+        color: "black",
+        weight: 0.5,
+        fillColor: getColor(location.coordinates[2]),
+        radius: usgsURL[i].properties.mag 
+      }).bindPopup("Magnitude: " + usgsURL[i].properties.mag
+        + "<br>Depth: " + location.coordinates[2]
+        + "<br>Location:" + usgsURL[i].properties.place)
+        .addTo(myMap);
+        }
+  }
+});
